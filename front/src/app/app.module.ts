6e0929c8c6b93d29accd3a8bc43892f8e72d236e
Cédric MILLET <milletcedric83400@gmail.com>
2020-11-16 17:43:03 +0100

    adds routing system

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//  Routing
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'home', component: MainComponent },
+  { path: '*', component: MainComponent },
+
+];
+
+//  App components
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
