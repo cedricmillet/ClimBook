856f0a2878384648b42a2a563a37374f9e1a750a
Cédric MILLET <milletcedric83400@gmail.com>
2020-11-20 18:15:18 +0100

    updates dump

--- database.sql
+++ database.sql
@@ -1,6 +1,7 @@
 --
 -- DROP DES EVENTUELLES TABLES EXISTANTES
 --
+
 DROP TABLE IF EXISTS chronos;
 DROP TABLE IF EXISTS ascensions;
 DROP TABLE IF EXISTS voies;
@@ -8,18 +9,26 @@ DROP TABLE IF EXISTS utilisateurs;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS niveaux;
 DROP EXTENSION IF EXISTS pgcrypto;
+DROP FUNCTION IF EXISTS getNiveauMoyen;
+DROP FUNCTION IF EXISTS getTempsMoyen;
+DROP FUNCTION IF EXISTS getBestGrimp;
+DROP FUNCTION IF EXISTS getNombreVal;
 --
 -- pgcrypto pour encoder les mot de passe
 --
 CREATE EXTENSION pgcrypto;
 
 
+/*-----------------------------------------
+                TABLES
+-----------------------------------------*/
+
 --
 -- CREATION TABLE >> NIVEAUX
 --
 CREATE TABLE niveaux (
     id SERIAL PRIMARY KEY,
-	difficulte INT, /* permet d'attribuer un score correspondant à un niveau */
+	difficulte INT,
     nom VARCHAR(50) NOT NULL,
     couleur VARCHAR(15) NOT NULL
 );
@@ -64,7 +73,6 @@ CREATE TABLE voies (
             ON DELETE SET NULL
 );
 
-
 --
 -- CREATION TABLE >> ASCENSIONS
 --
@@ -84,7 +92,6 @@ CREATE TABLE ascensions (
             ON DELETE SET NULL
 );
 
-
 --
 -- CREATION TABLE >> CHRONOS
 --
@@ -102,20 +109,17 @@ CREATE TABLE chronos (
             ON DELETE SET NULL
 );
 
-
 --
 -- AJOUT DES ROLES
 --
 INSERT INTO roles (id, role) VALUES(1, 'admin');
 INSERT INTO roles (id, role) VALUES(2, 'client');
 
---
 -- AJOUT DES UTILISATEURS
 --
 INSERT INTO utilisateurs (id,id_role, pseudo, mdp, email,isabonne) VALUES(1, 1, 'admin', crypt('admin', gen_salt('bf')), 'admin@domain.com',true);
 INSERT INTO utilisateurs (id,id_role, pseudo, mdp, email,isabonne) VALUES(2, 2, 'client', crypt('client', gen_salt('bf')), 'client@domain.com',false);
 
---
 -- AJOUT DES NIVEAUX
 --
 INSERT INTO niveaux (id,difficulte,nom,couleur) VALUES(1,3,'3A','blanc');
@@ -124,7 +128,6 @@ INSERT INTO niveaux (id,difficulte,nom,couleur) VALUES(3,8,'5A','bleu');
 INSERT INTO niveaux (id,difficulte,nom,couleur) VALUES(4,15,'6B','rouge');
 INSERT INTO niveaux (id,difficulte,nom,couleur) VALUES(5,18,'7B','noir');
 INSERT INTO niveaux (id,difficulte,nom,couleur) VALUES(6,25,'8A','violet');
-
 --
 -- AJOUT DES VOIES
 --
@@ -133,14 +136,117 @@ INSERT INTO voies (id,id_niveau,nom) VALUES(2,5,'The big Wall');
 INSERT INTO voies (id,id_niveau,nom) VALUES(3,2,'Un nouvel espoir');
 
 
---
 -- AJOUT DES ASCENSION
 --
-INSERT INTO ascensions (id,id_utilisateur,id_voie,temps) VALUES(1,2,2,'32:12.32');
-INSERT INTO ascensions (id,id_utilisateur,id_voie,temps) VALUES(2,1,2,'32:12.0');
+INSERT INTO ascensions (id,id_utilisateur,id_voie,temps) VALUES(1,2,1,'32:12.32');
+INSERT INTO ascensions (id,id_utilisateur,id_voie,temps) VALUES(2,2,2,'39:12.48');
+INSERT INTO ascensions (id,id_utilisateur,id_voie,temps) VALUES(2,2,3,'39:12.25');
+
 
 --
 -- AJOUT DES VALIDATIONS
 --
 INSERT INTO chronos (id,id_ascension,id_utilisateur) VALUES(1,1,2);
 INSERT INTO chronos (id,id_ascension,id_utilisateur) VALUES(2,2,1);
+
+
+
+/*-----------------------------------------
+                FONCTIONS
+-----------------------------------------*/
+
+
+-- Retourne le nombre de validations pour une ascencion donnée
+CREATE OR REPLACE FUNCTION
+   getNombreVal(id_a INTEGER)
+RETURNS Record AS $$
+    SELECT COUNT(*) FROM chronos
+    WHERE chronos.id_ascension = id_a
+    
+$$ LANGUAGE SQL;
+
+/* SELECT * FROM getNombreVal(2) AS (Nb INTEGER);*/
+
+-- Retourne le niveau moyen d'un utilisateur donné
+CREATE OR REPLACE FUNCTION getNiveauMoyen(idTestUtilisateur Integer) RETURNS record AS $$
+	SELECT AVG(difficulte) FROM Niveaux WHERE Niveaux.id IN (
+			SELECT id_niveau FROM Voies WHERE Voies.id IN ( 
+				SELECT id_voie FROM Ascensions WHERE Ascensions.id_Utilisateur = idTestUtilisateur));
+
+$$ LANGUAGE SQL;
+
+/*
+SELECT * FROM getNiveauMoyen(2) AS (Moyenne Integer);*/ --L'utilisateur 2 à effectué 2 ascension de difficultés 3 et 8, doonc retourne 5
+	
+-- Retourne le temps moyen sur une voie donnée
+CREATE OR REPLACE FUNCTION getTempsMoyen(idTestVoie Integer) RETURNS record AS $$		
+	SELECT AVG(temps) FROM ascensions,voies
+	WHERE voies.id = idTestVoie
+	AND ascensions.id_voie = voies.id	
+		
+$$ LANGUAGE SQL;
+
+/*SELECT * FROM getTempsMoyen(1) AS (Moyenne TIME);*/
+
+--  Calcule et retourne les 5 meilleurs grimpeurs d'une voie donnée
+CREATE OR REPLACE FUNCTION
+   getBestGrimp(id_v INTEGER)
+RETURNS SETOF record AS $$
+    SELECT utilisateurs.id , utilisateurs.pseudo FROM utilisateurs, ascensions, voies
+    WHERE voies.id = id_v
+    AND ascensions.id_voie = voies.id
+    AND utilisateurs.id = ascensions.id_utilisateur
+    ORDER BY ascensions.temps ASC LIMIT 5
+    
+$$ LANGUAGE SQL;
+
+/*
+SELECT * FROM getBestGrimp(1) AS (id_u INTEGER, pseudo_u VARCHAR);*/
+
+
+
+
+/*-----------------------------------------
+                TRIGGERS
+-----------------------------------------*/
+
+-- Avant de supprimer un utilisateur, on supprime ses statistiques (ascencions et validationsChrono)
+
+CREATE OR REPLACE FUNCTION supp_utilisateur()
+    RETURNS TRIGGER AS
+    $$
+    BEGIN
+        DELETE FROM ascensions WHERE ascensions.id_utilisateur = OLD.id;
+        DELETE FROM chronos WHERE chronos.id_utilisateur =  OLD.id;
+        RETURN OLD;
+    END;
+    $$
+    LANGUAGE PLPGSQL;
+
+
+CREATE TRIGGER supp_utilisateur
+    BEFORE DELETE
+    ON utilisateurs 
+    FOR EACH ROW
+EXECUTE PROCEDURE supp_utilisateur();
+
+--Faire en sorte que si on supprime un utilisateur 
+--on supprime les champs dans chronos et ascensions associé à l'id de l'utilisateur.
+
+CREATE OR REPLACE FUNCTION supp_utilisateur()
+    RETURNS TRIGGER AS
+    $$
+    BEGIN
+        DELETE FROM ascensions WHERE ascensions.id_utilisateur = OLD.id;
+        DELETE FROM chronos WHERE chronos.id_utilisateur =  OLD.id;
+        RETURN OLD;
+    END;
+    $$
+    LANGUAGE PLPGSQL;
+
+
+CREATE TRIGGER supp_utilisateur
+    BEFORE DELETE
+    ON utilisateurs 
+    FOR EACH ROW
+EXECUTE PROCEDURE supp_utilisateur();
